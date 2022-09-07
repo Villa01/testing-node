4841a3a6589c123f855db65019b48dd1bba66fca
Elder Pum <ElderPum@gmail.com>
2022-09-07 10:52:56 -0600

    Endpoint login terminado y testeado

--- Python/src/app.py
+++ Python/src/app.py
@@ -9,11 +9,11 @@ import boto3
 import base64
 import bcrypt
 from boto3.s3.transfer import S3Transfer
-from pathlib import Path 
+from pathlib import Path
 from io import BytesIO
 from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
-from werkzeug.datastructures import  FileStorage
+from werkzeug.datastructures import FileStorage
 from botocore.exceptions import ClientError
 from flask import jsonify
 
@@ -42,19 +42,6 @@ def page_not_found(error):
     return "<h1>Not found page</h1>", 404
 
 
-def validarPassword(user):
-    query = 'SELECT proyecto1.getUsuario(' + user + ')'
-    cur = connection.cursor()
-    cur.execute(query)
-    connection.commit()
-    res = cur.fetchall()
-    cur.close()
-    flag = False
-    if res:
-        flag = True
-    return flag
-
-
 def encrypt(word):
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(word, salt)
@@ -64,75 +51,61 @@ def compareEncrypted(string1, string2):
     return bcrypt.checkpw(string1, string2)
 
 
-def getUser(user):
-    query = 'SELECT * from proyecto1.getUsuario(' + user + ')'
-    cur = connection.cursor()
-    cur.execute(query)
-    connection.commit()
-    res = cur.fetchone()
-    id = None
-    if res:
-        id = res[0]
-    cur.close()
-    return id
-
-
-#@app.route('/login', methods=['POST'])
-#def login():
-#    user = request.json['username']
-#    password = encrypt(request.json['password'])
-#    cur = connection.cursor()
-#    query = 'SELECT proyecto1.getUsuario(' + user + ')'
-#    cur.execute(query)
-#    connection.commit()
-#    res = {}
-#    flag = True
-#    for username
-
-@app.route('/getUser', methods=['GET'])
-def getUsers():
+#LOGIN
+@app.route('/api/auth/', methods=['POST'])
+def login():
     try:
-        #Traemos desde el body el usuario
-        req = request.get_json()
-        userSearch = req['username']
+        # Obtenemos los datos que manda postman desde json
+        user = request.json['username']
+        password = request.json['password']
+        print(password)
 
-        #Ahora hacemos la búsqueda en la db
-        query = 'SELECT proyecto1.getUsuario($1)'
-        params = [userSearch]
+        # Procedemos a invocar el query de la db para validar el login
+        query = 'SELECT * from proyecto1.getUsuario(%s)'
+        params = [user]
         cur = connection.cursor()
         cur.execute(query, params)
+        print(cur)
         connection.commit()
-        rows = []
-        for user, password in cur.fetchall():
-            res = {}
-            res['username'] = user
-            res['password'] = password
-            rows.append(res)
-        cur.close()
-        return jsonify(rows)
+        response = {}
+        for id, username, email, passw, perfil in cur.fetchall():
+            if compareEncrypted(password.encode(), passw.encode()):
+                response['id'] = id
+                response['username'] = username
+                response['email'] = email
+                response['perfil'] = perfil
+                print("Usuario encontrado")
+            else:
+                response = {'message:' 'No se encontró al usuario asociado al username ' + user}
+                print("Usuario no encontrado")
 
+        cur.close()
+        connection.close()
+        return jsonify(response)
     except Exception as e:
-        response = {'message': 'No se encontro al usuario'}
         print(e)
+        response = {
+            'message': 'No se encontró al usuario ingresado, intente nuevamente'}
         return jsonify(response)
 
-@app.route('/crearUsuario', methods=['POST'])
+#CREAR USUARIOS
+@app.route('/api/users/', methods=['POST'])
 def createUser():
     load_dotenv()
 
     s3Client = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ID_PASSWORD'),
                             aws_secret_access_key=os.getenv('AWS_PASSWORD'))
     bucket = os.getenv('AWS_S3_NAME')
-    
+
     try:
-        #Obtenemos el body para poder agregar al usuario, menos la foto porque eso es aparte
+        # Obtenemos el body para poder agregar al usuario, menos la foto porque eso es aparte
         user = request.form['username']
         email = request.form['email']
         password = request.form['password']
         perfil = request.files['perfil']
         encryptedPass = encrypt(password.encode())
-        
-        #Mandamos a crear un folder llamado file donde meteremos las imagenes
+
+        # Mandamos a crear un folder llamado file donde meteremos las imagenes
         try:
             os.stat(os.path.dirname(__file__) + os.getenv('UPLOAD_PATH'))
         except:
@@ -140,25 +113,27 @@ def createUser():
 
         print("bandera")
 
-        #Vamos a obtener la ruta del archivo que vamos a subir
+        # Vamos a obtener la ruta del archivo que vamos a subir
         basepath = os.path.dirname(__file__)
-        uploadPath = os.path.join(basepath, os.getenv('UPLOAD_PATH').replace("/", ""), secure_filename(perfil.filename))
+        uploadPath = os.path.join(basepath, os.getenv('UPLOAD_PATH').replace(
+            "/", ""), secure_filename(perfil.filename))
         extension = "." + uploadPath.split('.')[-1]
         perfil.save(uploadPath)
         fotoUsuario = user + "-profile" + extension
 
-        #Ahora se envia al s3
+        # Ahora se envia al s3
         transfer = S3Transfer(s3Client)
         transfer.upload_file(uploadPath, bucket, fotoUsuario)
-        fileURL = '%s/%s/%s' % (s3Client.meta.endpoint_url, bucket, fotoUsuario)
+        fileURL = '%s/%s/%s' % (s3Client.meta.endpoint_url,
+                                bucket, fotoUsuario)
         os.remove(uploadPath)
         print(fileURL)
 
-        #Despues de subir al s3 agregamos a la db
+        # Despues de subir al s3 agregamos a la db
         query = 'CALL proyecto1.addUsuario(%s, %s, %s, %s, 0)'
-        params = [user, email, encryptedPass, fileURL]
+        params = [user, email, encryptedPass.decode("utf-8"), fileURL]
         cur = connection.cursor()
-        cur.execute(query, (user, email, encryptedPass.decode("utf-8"), fileURL))
+        cur.execute(query, params)
         connection.commit()
         cur.close()
         connection.close()
